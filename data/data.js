d8c656d86342d06c9bdbc669a79ae382f3222c90
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-09-14 11:52:02 +0100

    add query parameter filtering to startup API

--- data/data.js
+++ data/data.js
@@ -1,21 +1,3 @@
-export const people = [
-  {
-    name: "Lena",
-    job: "Architect",
-    isRich: true,
-  },
-  {
-    name: "Marcus",
-    job: "Barista",
-    isRich: false,
-  },
-  {
-    name: "Ava",
-    job: "Software Engineer",
-    isRich: true,
-  },
-];
-
 export const startups = [
   {
     id: 0,
--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 import express from "express";
-import { people, startups } from "./data/data.js";
+import { startups } from "./data/data.js";
 
 const PORT = 8000;
 
 const app = express();
 
 app.get("/api", (request, response) => {
-  console.log(request.query);
-  response.json(people);
+  let results = startups;
+
+  if (request.query.industry) {
+    results = results.filter(
+      (startup) =>
+        startup.industry.toLowerCase() === request.query.industry.toLowerCase(),
+    );
+  }
+  if (request.query.country) {
+    results = results.filter(
+      (startup) =>
+        startup.country.toLowerCase() === request.query.country.toLowerCase(),
+    );
+  }
+  if (request.query.continent) {
+    results = results.filter(
+      (startup) =>
+        startup.continent.toLowerCase() ===
+        request.query.continent.toLowerCase(),
+    );
+  }
+  if (request.query.is_seeking_funding === "true") {
+    results = results.filter((startup) => startup.is_seeking_funding === true);
+  }
+  if (request.query.is_seeking_funding === "false") {
+    results = results.filter((startup) => startup.is_seeking_funding === false);
+  }
+  if (request.query.has_mvp === "true") {
+    results = results.filter((startup) => startup.has_mvp === true);
+  }
+  if (request.query.has_mvp === "false") {
+    results = results.filter((startup) => startup.has_mvp === false);
+  }
+  response.json(results);
 });
 
-app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
