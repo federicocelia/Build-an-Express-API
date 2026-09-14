3770a3e40a67b8919b26d63c845151d867992ea3
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-09-14 09:22:47 +0100

    setting response

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const PORT = 8000;
 
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham",
+};
+
 const app = express();
+
+app.get("/", (request, response) => {
+  response.json(celebrity);
+});
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
