496770930ac84730a2199300650b0d4b0616cb4f
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-09-14 09:35:42 +0100

    Set GET response for API endpoint

--- server.js
+++ server.js
@@ -3,15 +3,10 @@ import { startups } from "./data/data.js";
 
 const PORT = 8000;
 
-const celebrity = {
-  type: "action hero",
-  name: "JSON Statham",
-};
-
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json(celebrity);
+app.get("/api", (request, response) => {
+  response.json(startups);
 });
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
